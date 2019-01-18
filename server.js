6ce1310f1028001f2256b01ecd8c48d5df5dbbb9
Nicolás Morales <nicomorales297@gmail.com>
2019-01-17 22:42:56 -0300

    hbs error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs')
 
-require('./hbs/helpers') // no se deja en constante porque el código se ejecuta solamente, cuando se importa
+require('./hbs/helpers/helpers') // no se deja en constante porque el código se ejecuta solamente, cuando se importa
 
 //usa la carpeta public como sitio web
 app.use(express.static(__dirname+'/public'))
@@ -19,7 +19,7 @@ app.set('view engine','hbs')
 //renders
 app.get('/',(req,res)=>{
     res.render('home',{
-        nombre:'nIcolás'//estas variables se pueden usar en el .hbs con {{}}, si se manda a llamar alguna que no esté acá, entonces se buscará en los helpers, como en el caso del getAnio del footer
+        nombre:'hUmano'//estas variables se pueden usar en el .hbs con {{}}, si se manda a llamar alguna que no esté acá, entonces se buscará en los helpers, como en el caso del getAnio del footer
     })
 })
 app.get('/about',(req,res)=>{
