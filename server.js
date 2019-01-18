4c30a627b31d37580e4d8efb1e2ead54abfb08a9
Nicolás Morales <nicomorales297@gmail.com>
2019-01-18 15:16:20 -0300

    dns

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs')
 const os = require('os')
+const dns = require('dns');
 
 require('./hbs/helpers/helpers') // no se deja en constante porque el código se ejecuta solamente, cuando se importa
 
@@ -13,10 +14,13 @@ app.use(express.static(__dirname+'/public'))
 hbs.registerPartials(__dirname + '/views/parciales')
 //engine que renderiza archivos hbs
 app.set('view engine','hbs')
-
+dns.lookup('iana.org', (err, address, family) => {
+    console.log('address: %j family: IPv%s', address, family);
+  });
 //renders
 app.get('/',(req,res)=>{
     const cpu = os.cpus()
+  
     res.render('home',{
         nombre:os.hostname(),//estas variables se pueden usar en el .hbs con {{}}, si se manda a llamar alguna que no esté acá, entonces se buscará en los helpers, como en el caso del getAnio del footer
         tipo: os.type(),
